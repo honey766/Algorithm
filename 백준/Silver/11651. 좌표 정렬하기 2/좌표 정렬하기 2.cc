203e3a5b766437711d6e842f4ce6d57b85d80d54
honey766 <157039799+honey766@users.noreply.github.com>
2024-01-19 00:47:59 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 48 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct coordinate {
+    int x;
+    int y;
+};
+
+bool incr(coordinate a, coordinate b) {
+    if (a.y == b.y) return a.x < b.x;
+    else return a.y < b.y;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, x, y;
+    cin >> n;
+    coordinate *a = new coordinate[n];
+    for (int i = 0; i < n; i++) {
+        cin >> x >> y;
+        a[i].x = x; a[i].y = y;
+    }
+    sort(a, a + n, incr);
+    for (int i = 0; i < n; i++) cout << a[i].x << ' ' << a[i].y << '\n';
+}
