0f3d45f35525f0e1795b980aa2c9d0eb24156331
honey766 <157039799+honey766@users.noreply.github.com>
2024-01-19 00:38:30 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    a += c / 60;
+    b += c % 60;
+    if(b >= 60){
+        b -= 60;
+        a += 1;
+    }
+    if(a >= 24)
+        a = a % 24;
+    cout << a << " " << b;
+}
