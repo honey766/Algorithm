1f5ed7822361662fd4a036899551a1511b04fa68
honey766 <157039799+honey766@users.noreply.github.com>
2024-01-19 00:43:03 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b;
+    while (cin >> a >> b) {
+        if (a == 0 && b == 0) break;
+        if (a % b == 0) cout << "multiple";
+        else if (b % a == 0) cout << "factor";
+        else cout << "neither";
+        cout << '\n';
+    }
+}
