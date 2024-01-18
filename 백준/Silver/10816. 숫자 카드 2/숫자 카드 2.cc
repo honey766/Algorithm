3c16bae526904173969e54002de2defd8d9493fa
honey766 <157039799+honey766@users.noreply.github.com>
2024-01-19 00:46:02 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 220 ms, Memory: 9916 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int SIZE = 1009997;
+int NIL = 10000001;
+
+int f1(int n) {
+    return (n + 10000000) % SIZE;
+}
+
+int f2(int n) {
+    return 1 + ((n + 10000000) % 1009991);
+}
+
+int insert_hash(int* hash, int* c, int n) {
+    int h;
+    for (int i = 0; i < SIZE / 2; i++) {
+        h = (f1(n) + f2(n) * i) % SIZE;
+        if (hash[h] == n) {
+            c[h]++; return 1;
+        }
+        if (hash[h] == NIL) {
+            hash[h] = n; c[h]++; return 1;
+        }
+    }
+    return -1;
+}
+
+int search_hash(int* hash, int* c, int n) {
+    int h;
+    for (int i = 0; i < SIZE / 2; i++) {
+        h = (f1(n) + f2(n) * i) % SIZE;
+        if (hash[h] == NIL)
+            return 0;
+        if (hash[h] == n)
+            return c[h];
+    }
+    return 0;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int n, a;
+    cin >> n;
+    int* hash = new int[SIZE];
+    int* c = new int[SIZE]();
+    for (int i = 0; i < SIZE; i++) hash[i] = NIL;
+    for (int i = 0; i < n; i++) {
+        cin >> a; insert_hash(hash, c, a);
+    }
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        cout << search_hash(hash, c, a) << ' ';
+    }
+}
