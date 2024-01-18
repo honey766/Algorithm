ef9921e82d2306b162af955285e4a9740b625a2a
honey766 <157039799+honey766@users.noreply.github.com>
2024-01-19 02:29:08 +0900

    [D4] Title: 사탕 가방, Time: 13 ms, Memory: 13,668 KB -BaekjoonHub

---
+++ SWEA/D4/11446. 사탕 가방/사탕 가방.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+bool f(long long* a, int size, long long n, long long m) {
+    if (n == 0) return true;
+    for (int i = 0; i < size; i++) {
+        if (a[i] < n) continue;
+        m -= a[i] / n;
+        if (m <= 0) return true;
+    }
+    return false;
+}
+
+int main(int argc, char** argv)
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+	int test_case;
+	int T;
+	/*
+	   아래의 freopen 함수는 input.txt 를 read only 형식으로 연 후,
+	   앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+	   //여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+	   freopen 함수를 이용하면 이후 cin 을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+	   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 함수를 사용하셔도 좋습니다.
+	   freopen 함수를 사용하기 위해서는 #include <cstdio>, 혹은 #include <stdio.h> 가 필요합니다.
+	   단, 채점을 위해 코드를 제출하실 때에는 반드시 freopen 함수를 지우거나 주석 처리 하셔야 합니다.
+	*/
+	//freopen("input.txt", "r", stdin);
+	cin>>T;
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+        cout << '#' << test_case << ' ';
+		long long n, m;
+        cin >> n >> m;
+        long long* a = new long long[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        long long left, right, mid, ans;
+        left = 0; right = 1e18;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (f(a, n, mid, m)) { //가방 개수가 mid개가 가능하다면
+                left = mid + 1; //오른쪽을 본다
+                ans = mid;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        cout << ans << '\n';
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
