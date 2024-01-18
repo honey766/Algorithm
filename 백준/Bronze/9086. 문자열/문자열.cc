5ae1fe98c004fb501473f3f6de7b3294233814f9
honey766 <157039799+honey766@users.noreply.github.com>
2024-01-19 00:44:17 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    string s;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> s;
+        cout << s.front() << s.back() << '\n';
+    }
+}
