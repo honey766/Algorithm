045f6dfb00aca2e6f2df59c6ca1784be9b9e1482
honey766 <157039799+honey766@users.noreply.github.com>
2024-01-28 13:57:59 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 144 ms, Memory: 10220 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,36 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int arr[1025][1025];
-int rowSum[1025][1025];
-int colSum[1025][1025];
+int arr[1024][1024];
+int sum[1025][1025];
 
 int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-    int n, m, x1, y1, x2, y2, sum;
+    int n, m, x1, y1, x2, y2;
     cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> arr[i][j];
+    for (int i = 0; i <= n; i++)
+        sum[i][0] = sum[0][i] = 0;
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= n; j++)
-            cin >> arr[i][j];
-    for (int i = 1; i <= n; i++) {
-        rowSum[i][0] = 0;
-        colSum[0][i] = 0;
-        for (int j = 1; j <= n; j++) {
-            rowSum[i][j] = rowSum[i][j - 1] + arr[i][j]; //ith 행에서 j번째까지 더함
-            colSum[j][i] = colSum[j - 1][i] + arr[j][i]; //ith 열에서 j번째까지 더함
-        }
-    }
+            sum[i][j] = arr[i - 1][j - 1] + sum[i][j - 1] + sum[i - 1][j] - sum[i - 1][j - 1];
     while (m--) {
-        sum = 0;
         cin >> x1 >> y1 >> x2 >> y2;
-        if(x2 - x1 < y2 - y1)
-            for (int i = x1; i <= x2; i++)
-                sum += rowSum[i][y2] - rowSum[i][y1 - 1];
-        else
-            for (int i = y1; i <= y2; i++)
-                sum += colSum[x2][i] - colSum[x1 - 1][i];
-        cout << sum << '\n';
+        cout << sum[x2][y2] - sum[x2][y1 - 1] - sum[x1 - 1][y2] + sum[x1 - 1][y1 - 1] << '\n';
     }
 }
