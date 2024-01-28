f09f953c88339d71fb3618f0aa99997e8bfcada8
honey766 <157039799+honey766@users.noreply.github.com>
2024-01-28 14:21:43 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    int a, n; cin >> n;
+    priority_queue<int> pq;
+    while (n--) {
+        cin >> a;
+        if (a == 0) {
+            if (pq.empty()) cout << "0\n";
+            else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        }
+        else pq.push(a);
+    }
+}
