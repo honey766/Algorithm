6810378a8c2fb60e2e254af7a96284c6840a8a57
honey766 <157039799+honey766@users.noreply.github.com>
2024-02-06 03:42:46 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    int h, m, s, t;
+    cin >> h >> m >> s >> t;
+    s += t;
+    m += s / 60;
+    s %= 60;
+    h += m / 60;
+    m %= 60;
+    h %= 24;
+    cout << h << ' ' << m << ' ' << s;
+}
