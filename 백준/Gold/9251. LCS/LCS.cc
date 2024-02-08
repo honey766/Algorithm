8378c303c91bf127ff46d68779208741a460a172
honey766 <157039799+honey766@users.noreply.github.com>
2024-02-09 00:40:31 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int dp[1001][1001]; //dp[i][j] : 각 문자열의 i-1, j-1번째까지 봤을 때 LCS
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	string s1, s2;
+	cin >> s1 >> s2;
+	int n = s1.size();
+	int m = s2.size();
+
+	for (int i = 0; i < n; ++i) 
+		dp[i][0] = 0;
+	for (int i = 0; i < m; ++i)
+		dp[0][i] = 0;
+
+	for (int i = 1; i <= n; ++i)
+		for (int j = 1; j <= m; ++j) {
+			if (s1[i - 1] == s2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+			else dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+		}
+	cout << dp[n][m];
+}
