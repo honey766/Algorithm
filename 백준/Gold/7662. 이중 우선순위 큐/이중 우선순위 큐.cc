a82632d5860ff877866da04b3f85e4ab95566c12
honey766 <157039799+honey766@users.noreply.github.com>
2024-02-11 01:09:07 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 660 ms, Memory: 16312 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -1,41 +1,54 @@
 #include <iostream>
-#include <map>
+#include <queue>
 using namespace std;
 
 int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-    map<int, int> maps;
     char c;
     int n, a, tc; 
     cin >> tc;
     while (tc--) {
-        maps.clear();
+        //최대힙, 최소힙에 둘 다 넣고 삭제 연산은 둘 중에 하나에서 뺀다
+        //최대힙에서 뺀다면, 최소힙에서는 그 원소가 남아있으면 안 된다. 이러한 원소를 mintrash에 담는다
+        priority_queue<int> maxpq;
+        priority_queue<int> maxtrash;
+        priority_queue<int, vector<int>, greater<int>> minpq;
+        priority_queue<int, vector<int>, greater<int>> mintrash;
         cin >> n;
         while (n--) {
             cin >> c >> a;
-            if (c == 'I')
-                if (maps.count(a) == 0)
-                    maps.emplace(a, 1);
-                else
-                    ++maps[a];
-            else if (maps.empty())
+            if (c == 'I') {
+                maxpq.push(a); minpq.push(a);
+            }
+            else if (maxpq.size() + minpq.size() == maxtrash.size() + mintrash.size())
                 continue;
+            else if(a == 1){
+                while (!maxtrash.empty() && maxtrash.top() == maxpq.top()) {
+                    maxtrash.pop(); maxpq.pop();
+                }
+                mintrash.push(maxpq.top());
+                maxpq.pop();
+            }
             else {
-                pair<int, int> p;
-                if (a == 1) p = *maps.rbegin();
-                else p = *maps.begin();
-                if (p.second == 1)
-                    maps.erase(p.first);
-                else
-                    --maps[p.first];
+                while (!mintrash.empty() && mintrash.top() == minpq.top()) {
+                    mintrash.pop(); minpq.pop();
+                }
+                maxtrash.push(minpq.top());
+                minpq.pop();
             }
         }
 
-        if (maps.empty())
+        while (!maxtrash.empty() && maxtrash.top() == maxpq.top()) {
+            maxtrash.pop(); maxpq.pop();
+        }
+        while (!mintrash.empty() && mintrash.top() == minpq.top()) {
+            mintrash.pop(); minpq.pop();
+        }
+        if (maxpq.size() + minpq.size() == maxtrash.size() + mintrash.size())
             cout << "EMPTY\n";
         else
-            cout << (*maps.rbegin()).first << ' ' << (*maps.begin()).first << '\n';
+            cout << maxpq.top() << ' ' << minpq.top() << '\n';
     }
 }
