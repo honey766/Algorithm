7fd4240f0be35db8b7cfc076f7217439042706d4
honey766 <157039799+honey766@users.noreply.github.com>
2024-02-15 05:33:22 +0900

    [Gold V] Title: CCW, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold V/11758. CCW/CCW.cc
+++ 백준/Gold V/11758. CCW/CCW.cc
@@ -1,14 +1,3 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main()
-{
-    ios_base::sync_with_stdio(0); cin.tie(0);
-
-    int x[3], y[3];
-    for (int i = 0; i < 3; ++i)
-        cin >> x[i] >> y[i];
-    int ccw = (x[1] - x[0]) * (y[2] - y[1]) - (x[2] - x[1]) * (y[1] - y[0]);
-    if (!ccw) cout << 0;
-    else cout << (ccw > 0 ? 1 : -1);
-}
+int main(){int a,b,c,d,e,f,w;cin>>a>>d>>b>>e>>c>>f;w=(b-a)*(f-e)-(c-b)*(e-d);cout<<(!w?0:w>0?1:-1);}
