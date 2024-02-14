8e3c43b3afd1eea416e70e3f7171d7a294464004
honey766 <157039799+honey766@users.noreply.github.com>
2024-02-15 05:47:09 +0900

    [Gold V] Title: CCW, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold V/11758. CCW/CCW.cc
+++ 백준/Gold V/11758. CCW/CCW.cc
@@ -1,2 +1,2 @@
 #import<iostream>
-main(){int a,b,c,d,e,f,w;std::cin>>a>>d>>b>>e>>c>>f;w=(b-a)*(f-e)-(c-b)*(e-d);std::cout<<(!w?0:w>0?1:-1);}
+main(){int a,b,c,d,e,f,w;std::cin>>a>>d>>b>>e>>c>>f;w=(b-a)*(f-e)-(c-b)*(e-d);std::cout<<(w?w>0?1:-1:0);}
