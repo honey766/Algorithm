840e2e0e65c979c40e7a2790ebe454e3149632d4
honey766 <157039799+honey766@users.noreply.github.com>
2024-02-18 22:55:00 +0900

    [Silver III] Title: 주유소, Time: 12 ms, Memory: 2412 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	int n, a, Min = 1'000'000'000;
+	long long sum = 0;
+	cin >> n;
+	int* len = new int[n - 1];
+	for (int i = 0; i < n - 1; i++)
+		cin >> len[i];
+	for (int i = 0; i < n - 1; i++) {
+		cin >> a;
+		Min = min(Min, a); //지금까지의 가장 싼 가격
+		sum += Min * len[i];
+	}
+	cin >> a;
+	cout << sum;
+}
