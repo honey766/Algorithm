1c86f3d72b22f7688410d9cec611c51b330bacd9
honey766 <157039799+honey766@users.noreply.github.com>
2024-02-20 05:05:56 +0900

    [Gold III] Title: 양팔저울, Time: 0 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.cc
+++ 백준/Gold/2629. 양팔저울/양팔저울.cc
@@ -3,7 +3,7 @@ using namespace std;
 #define f(j) ((j) % 2)
 
 short arr[30];
-bool dp[40001][2]; // dp[i][j % 2] : 숫자 i를 0~j번째 추로 측정할 수 있음
+bool dp[15001][2]; // dp[i][j] : 숫자 i를 0~j번째 추로 측정할 수 있음
 
 int main()
 {
@@ -11,23 +11,22 @@ int main()
 
 	int n1, n2, a;
 	cin >> n1;
-	for (int i = 0; i < n1; i++) {
+	for (int i = 0; i < n1; i++)
 		cin >> arr[i];
-	}
+        
 	dp[0][0] = dp[0][1] = dp[arr[0]][0] = true;
-	for (int j = 1; j < n1; j++) {
-		for (int i = 1; i <= 40000; i++) {
+	for (int j = 1; j < n1; j++)
+		for (int i = 1; i <= 15000; i++) {
 			a = (i > arr[j] ? i - arr[j] : arr[j] - i);
 			dp[i][f(j)] = dp[i][f(j - 1)] || dp[a][f(j - 1)];
-			if (i + arr[j] <= 40000)
+			if (i + arr[j] <= 15000)
 				dp[i][f(j)] = dp[i][f(j)] || dp[i + arr[j]][f(j - 1)];
 		}
-	}
 
 	cin >> n2;
 	for (int i = 0; i < n2; i++) {
 		cin >> a;
-		if (dp[a][f(n1 - 1)]) cout << "Y ";
-		else cout << "N ";
+		if(a > 15000 || !dp[a][f(n1 - 1)]) cout << "N ";
+		else cout << "Y ";
 	}
 }
