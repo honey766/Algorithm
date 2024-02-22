fb08cf1cb3ccb89d929057cd37ba1a02c0ce5a38
honey766 <157039799+honey766@users.noreply.github.com>
2024-02-22 18:17:19 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.cc
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -1,21 +1,2 @@
-#include <iostream>
-using namespace std;
-
-int dp[10001];
-
-int main()
-{
-	ios_base::sync_with_stdio(false); cin.tie(NULL);
-
-	int n, k, a;
-	cin >> n >> k;
-	dp[0] = 1;
-	while(n--) {
-		cin >> a;
-		//해당 동전을 쓰는 경우를, i-1번째까지 동전만 쓰는 경우의 수에 더해간다.
-		//해당 동전(a원짜리)을 1개 이상 써서 j원을 만드는 경우의 수는 dp[j - a]이다.
-		for (int j = a; j <= k; j++)
-		    dp[j] += dp[j - a];
-	}
-	cout << dp[k];
-}
+#import<iostream>
+int d[10001];main(){int n,k,a,j;std::cin>>n>>k;d[0]=1;while(n--){std::cin>>a;for(j=a;j<=k;j++)d[j]+=d[j-a];}std::cout<<d[k];}
