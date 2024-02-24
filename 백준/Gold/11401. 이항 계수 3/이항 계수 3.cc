bb2ac02718758a369b1b432b78ef10349eacc984
honey766 <157039799+honey766@users.noreply.github.com>
2024-02-25 05:15:47 +0900

    [Gold I] Title: 이항 계수 3, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11401. 이항 계수 3/이항 계수 3.cc
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.cc
@@ -7,16 +7,15 @@ int main()
 	ios_base::sync_with_stdio(false); cin.tie(NULL);
 
 	int n, k, i;
-	long long a1, a2, a3, ret;
+	long long a1, a2, ret, aa[2] = { 1, 1 };
 	cin >> n >> k;
-	a1 = a2 = a3 = ret = 1;
-	for (i = 2; i <= n; i++)
+	a1 = ret = 1;
+	for (i = 2; i <= n; i++) {
 		a1 = (a1 * i) % N;
-	for (i = 2; i <= k; i++)
-		a2 = (a2 * i) % N;
-	for (i = 2; i <= n - k; i++)
-		a3 = (a3 * i) % N;
-	a2 = (a2 * a3) % N;
+		if (i == k) aa[0] = a1;
+		if (i == n - k) aa[1] = a1;
+	}
+	a2 = (aa[0] * aa[1]) % N;
 	//구하려는 것은 (a1 / a2) % N
 	// == (a1 * (a2 ^ (N-2))) % N
 
