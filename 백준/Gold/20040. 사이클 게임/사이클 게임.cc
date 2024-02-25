80099f6ab1e5797975c9dd727f7af57cd245065e
honey766 <157039799+honey766@users.noreply.github.com>
2024-02-25 18:35:07 +0900

    [Gold IV] Title: 사이클 게임, Time: 100 ms, Memory: 5932 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.cc
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.cc
@@ -2,6 +2,7 @@
 using namespace std;
 
 int* parent;
+int* Rank;
 
 int Find(int a) {
     if (a == parent[a]) return a;
@@ -13,8 +14,12 @@ bool Union(int a, int b) {
     int B = Find(b);
 
     if (A == B) return true;
-
-    parent[A] = B;
+    if (Rank[A] < Rank[B]) parent[A] = B;
+    else if (Rank[A] > Rank[B]) parent[B] = A;
+    else {
+        parent[B] = A;
+        Rank[A]++;
+    }
     return false;
 }
 
@@ -25,6 +30,7 @@ int main()
     int n, m, a, b;
     cin >> n >> m;
     parent = new int[n];
+    Rank = new int[n]();
     for (int i = 0; i < n; i++)
         parent[i] = i;
     for (int i = 1; i <= m; i++) {
