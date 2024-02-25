ea8c1a8d57964c9ad5d5ce1da391ca04218159da
honey766 <157039799+honey766@users.noreply.github.com>
2024-02-26 00:21:21 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 84 ms, Memory: 8840 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+vector<vector<int>> graph;
+int* visited;
+int cnt;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int n, m, r, u, v;
+    cin >> n >> m >> r;
+    graph.resize(n);
+    visited = new int[n]();
+    cnt = 0;
+    while (m--) {
+        cin >> u >> v;
+        graph[u - 1].push_back(v - 1);
+        graph[v - 1].push_back(u - 1);
+    }
+    for (int i = 0; i < n; i++)
+        sort(graph[i].begin(), graph[i].end(), greater<int>());
+
+    queue<int> que;
+    que.push(r - 1);
+    visited[r - 1] = ++cnt;
+    while (!que.empty()) {
+        int a = que.front();
+        que.pop();
+        for (int v : graph[a])
+            if (!visited[v]) {
+                que.push(v);
+                visited[v] = ++cnt;
+            }
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << visited[i] << '\n';
+}
