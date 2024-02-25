071d418ac53a9fba8b21ea62ea032b80c5398f0b
honey766 <157039799+honey766@users.noreply.github.com>
2024-02-26 00:15:48 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 88 ms, Memory: 10928 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> graph;
+int* visited;
+int cnt;
+void dfs(int r) {
+    visited[r] = ++cnt;
+    for (int v : graph[r]) {
+        if (!visited[v])
+            dfs(v);
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int n, m, r, u, v;
+    cin >> n >> m >> r;
+    graph.resize(n);
+    visited = new int[n]();
+    cnt = 0;
+    while (m--) {
+        cin >> u >> v;
+        graph[u - 1].push_back(v - 1);
+        graph[v - 1].push_back(u - 1);
+    }
+    for (int i = 0; i < n; i++)
+        sort(graph[i].begin(), graph[i].end());
+    dfs(r - 1);
+    for (int i = 0; i < n; i++)
+        cout << visited[i] << '\n';
+}
