91c38957c6a505f8b69d4b2a251fb51d5334cb5b
honey766 <157039799+honey766@users.noreply.github.com>
2024-02-26 20:41:32 +0900

    [Gold IV] Title: 플로이드, Time: 24 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+#define INF 1'000'000'000
+
+int w[100][100];
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int n, m, a, b, c;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            w[i][j] = INF;
+    while (m--) {
+        cin >> a >> b >> c;
+        a--; b--;
+        w[a][b] = min(w[a][b], c);
+    }
+    for (int i = 0; i < n; i++)
+        w[i][i] = 0;
+
+    for (int k = 0; k < n; k++)
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                w[i][j] = min(w[i][j], w[i][k] + w[k][j]);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (w[i][j] == INF) cout << "0 ";
+            else cout << w[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
