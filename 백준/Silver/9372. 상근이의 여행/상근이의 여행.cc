036843c7505573c0708600a2413cf93721cef617
honey766 <157039799+honey766@users.noreply.github.com>
2024-02-27 17:20:11 +0900

    [Silver IV] Title: 상근이의 여행, Time: 16 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -1,18 +1,16 @@
 #include <iostream>
-#include <vector>
-#include <string>
 using namespace std;
 
 int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
-
-    int t, n, m, a, b;
+    int t, n, m;
+    string s;
     cin >> t;
     while (t--) {
         cin >> n >> m;
         while (m--)
-            cin >> a >> b;
+            cin >> s >> s;
         cout << n - 1 << '\n';
     }
 }
