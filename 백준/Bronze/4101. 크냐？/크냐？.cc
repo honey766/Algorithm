da636a49a1502229be6e3112c10dfee9027f26a5
honey766 <157039799+honey766@users.noreply.github.com>
2024-03-18 01:06:15 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	int a, b;
+	while (cin >> a >> b) {
+		if (a == 0 && b == 0) break;
+		if (a > b) cout << "Yes\n";
+		else cout << "No\n";
+	}
+}
