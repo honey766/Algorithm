166ea87c35a904fb05f3b2aec99615d4fdbf0de1
honey766 <157039799+honey766@users.noreply.github.com>
2024-11-30 16:04:42 +0900

    [Silver II] Title: 사탕 게임, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.cc
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.cc
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-#define pr pair<int, int>
 
 int n;
 
@@ -22,24 +21,8 @@ int countRowMax(char** a, int i){
 	return maxCount;
 }
 
-// with input p, adjust the content of rowMax[3]
-void adjustRowMax(pr* rowMax, pr p){
-	if(rowMax[0] < p){
-		rowMax[2] = rowMax[1];
-		rowMax[1] = rowMax[0];
-		rowMax[0] = p;
-	}
-	else if(rowMax[1] < p){
-		rowMax[2] = rowMax[1];
-		rowMax[1] = p;
-	}
-	else if(rowMax[2] < p){
-		rowMax[2] = p;
-	}
-}
-
 // swap the candy a[i][j] and a[i][j+1], and count the max number of candy
-int swapAndCountMax(char** a, char** aT, pr* rowMax, pr* colMax, int i, int j){
+int swapAndCountMax(char** a, char** aT, int i, int j){
 	if(a[i][j] == a[i][j+1]) return 1; // can't change
 
 	swap(a[i][j], a[i][j+1]);
@@ -50,12 +33,6 @@ int swapAndCountMax(char** a, char** aT, pr* rowMax, pr* colMax, int i, int j){
 	int c2 = countRowMax(aT, j);
 	int c3 = countRowMax(aT, j+1);
 	int maxCount = max(max(c1, c2), c3);
-	for(int k = 0; k < 3; k++){
-		if(rowMax[k].second != i)
-			maxCount = max(maxCount, rowMax[k].first);
-		if(colMax[k].second != j && colMax[k].second != j+1)
-			maxCount = max(maxCount, colMax[k].first);
-	}
 
 	swap(a[i][j], a[i][j+1]);
 	swap(aT[j][i], aT[j+1][i]);
@@ -80,27 +57,15 @@ int main() {
 		}
 	}
 
-	// preprocessing
-	pr rowMax[3], colMax[3]; // (candyNum, index), rowMax[i] = (i+1)th largest number of candy row (if not changed the candy)
-	for(int i = 0; i < 3; i++)
-		rowMax[i] = colMax[i] = {-1, -1};
-	for(int i = 0; i < n; i++){
-		// horizontal
-		int count = countRowMax(a, i);
-		adjustRowMax(rowMax, {count, i});
-		// vertical
-		count = countRowMax(aT, i);
-		adjustRowMax(colMax, {count, i});
-	}
-
-	// count the max number of candy
 	int maxCount = 1;
+	for(int i = 0; i < n; i++){		
+		maxCount = max(maxCount, countRowMax(a, i)); // horizontal		
+		maxCount = max(maxCount, countRowMax(aT, i)); // vertical
+	}
 	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n-1; j++){
-			// horizontal
-			maxCount = max(maxCount, swapAndCountMax(a, aT, rowMax, colMax, i, j));
-			// vertical
-			maxCount = max(maxCount, swapAndCountMax(aT, a, colMax, rowMax, i, j));
+		for(int j = 0; j < n-1; j++){			
+			maxCount = max(maxCount, swapAndCountMax(a, aT, i, j)); // horizontal
+			maxCount = max(maxCount, swapAndCountMax(aT, a, i, j)); // vertical
 		}
 	}
 	cout << maxCount;
